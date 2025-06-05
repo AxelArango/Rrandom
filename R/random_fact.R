cdda76f27bd23f48f402965012679de8c5849a48
Axel Arango <110843705+AxelArango@users.noreply.github.com>
2025-06-05 16:34:42 +0200

    first push

---
+++ R/random_fact.R
@@ -0,0 +1,28 @@
+#' Random fact
+#' 
+#' This function provides the first or the first two paragraphs of a random wikipedia page.
+#' @return Two paragraphs of a random wikipedia page
+#' @export
+random_fact<-function(){
+  url <- "https://en.wikipedia.org/wiki/Special:Random" 
+  webpage <- rvest::read_html(url)
+  paragraphs <- webpage %>%
+    rvest::html_nodes("p")
+  if (length(paragraphs) >= 2) {
+    first_paragraph_text <- paragraphs[1] %>%
+      rvest::html_text(trim = TRUE) 
+    second_paragraph_text <- paragraphs[2] %>%
+      rvest::html_text(trim = TRUE)
+    cat("Random fact: \n")
+    cat(first_paragraph_text, "\n\n")
+    cat("")
+    cat(second_paragraph_text, "\n")
+  } else if (length(paragraphs) == 1) {
+    first_paragraph_text <- paragraphs[1] %>%
+      rvest::html_text(trim = TRUE)
+    cat("Random fact:\n")
+    cat(first_paragraph_text, "\n")
+  } else {
+    cat("No paragraphs found on the page.\n")
+  }
+}
